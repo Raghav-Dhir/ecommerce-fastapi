3a43dfe6f9003ac7956df347b3a45e6690263ee2
Raghav Dhir <raghavdhir01.rd@gmail.com>
2023-02-10 13:53:41 +0530

    user login and img uplaod

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
-from fastapi import FastAPI, Request, HTTPException, status
+from fastapi import FastAPI, Request, HTTPException, status, Depends, File, UploadFile
 from tortoise.contrib.fastapi import register_tortoise
 from models import *
-from authentication import get_hashed_password, verify_token
+from authentication import *
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from tortoise.signals import post_save
 from typing import List, Optional, Type
 from tortoise import BaseDBAsyncClient
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from emails import *
+from fastapi.staticfiles import StaticFiles
+from PIL import Image
+import secrets
 
 app = FastAPI()
+
+oauth2_schema = OAuth2PasswordBearer(tokenUrl='token')
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.post('/token')
+async def generate_token(request_form: OAuth2PasswordRequestForm = Depends()):
+    token = await token_generator(request_form.username, request_form.password)
+    return {"access_token": token, "token_type": "bearer"}
+
+async def get_current_user(token: str = Depends(oauth2_schema)):
+    try:
+        payload = jwt.decode(token, config_credentials["SECRET"], algorithms=['HS256'])
+        user = await User.get(id= payload.get("id"))
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    return await user
+
+@app.post('/user/me')
+async def user_login(user: user_pydanticIn = Depends(get_current_user)):
+    business = await Business.get(owner=user)
+    return {
+        "status" : "ok",
+        "data" : {
+            "username" : user.username,
+            "email" : user.email,
+            "verified" : user.is_verified,
+            "join_date" : user.join_date.strftime("%b %d %Y")
+        }
+    }
+
 
 @post_save(User)
 async def create_business(
@@ -59,6 +98,83 @@ async def email_verification(request: Request, token: str):
             detail="Invalid token or expired token",
             headers={"WWW-Authenticate": "Bearer"}
         )
+
+@app.post("/uploadfile/profile")
+async def create_upload_file(file: UploadFile = File(...), user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/images"
+    filename = file.filename
+    extension = filename.split(".")[1]
+    if extension not in ["png", "jpg"]:
+        return {"status": "error", "detail" : "File Extension not allowed"}
+
+    token_name = secrets.token_hex(10) + "." + extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+    
+    img = Image.open(generated_name)
+    img.resize(size=(200, 200))
+    img.save(generated_name)
+
+    file.close()
+
+    business = await Business.get(owner = user)
+    owner = await business.owner
+
+    if owner == user:
+        business.logo = token_name
+        await business.save()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated to perform this operation",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    file_url = "localhost:8000" + generated_name[1:]
+    return {
+        "status" : "ok",
+        "file name": {file_url}
+    }
+
+@app.post("/uploadfile/product/{id}")
+async def create_upload_file(id: int, file: UploadFile = File(...), user: user_pydantic = Depends(get_current_user)):
+    FILEPATH = "./static/images"
+    filename = file.filename
+    extension = filename.split(".")[1]
+    if extension not in ["png", "jpg"]:
+        return {"status": "error", "detail" : "File Extension not allowed"}
+
+    token_name = secrets.token_hex(10) + "." + extension
+    generated_name = FILEPATH + token_name
+    file_content = await file.read()
+    with open(generated_name, "wb") as file:
+        file.write(file_content)
+    
+    img = Image.open(generated_name)
+    img.resize(size=(200, 200))
+    img.save(generated_name)
+
+    file.close()
+
+    product = await Product.get(id = id)
+    business = await product.business
+    owner = await business.owner
+
+    if owner == user:
+        product.product_image = token_name
+        await product.save()
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authenticated to perform this operation",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+    file_url = "localhost:8000" + generated_name[1:]
+    return {
+        "status" : "ok",
+        "file name": {file_url}
+    }
 
 register_tortoise(
     app, 
